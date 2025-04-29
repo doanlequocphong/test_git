2334f80a055d3d10958d255d6c1f09419a70af28
doanlequocphong <doanlequocphong90@gmail.com>
2025-04-29 08:44:31 +0700

    [add] search file

---
+++ search.tsx
@@ -0,0 +1,21 @@
+import Form from 'next/form'
+ 
+export default function Page() {
+  return (
+    <Form action="/search">
+      {/* On submission, the input value will be appended to
+          the URL, e.g. /search?query=abc */}
+      <input name="query" />
+      <button type="submit">Submit Form Search</button>
+    </Form>
+  )
+}
+
+export default function Page() {
+    return (
+      <Form action="/search">
+        <input name="query" />
+        <button type="submit">Submit</button>
+      </Form>
+    )
+}
